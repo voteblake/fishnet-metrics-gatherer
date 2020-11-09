9fd2e27ac718f3175332d1362c0d2930470ac5f5
Blake Johnson <johnsonblake1@gmail.com>
2020-11-08 23:37:40 -0800

    Basic lambda echo

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use lambda::{handler_fn, Context};
+use serde_json::Value;
+
+type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let func = handler_fn(func);
+    lambda::run(func).await?;
+    Ok(())
+}
+
+async fn func(event: Value, _: Context) -> Result<Value, Error> {
+    Ok(event)
 }
