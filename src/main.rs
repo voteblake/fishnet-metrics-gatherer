3123ab33b586d7154091c2c7ce2751f2899cbd74
Blake Johnson <johnsonblake1@gmail.com>
2020-11-09 01:08:58 -0800

    first attempt at calling API

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,46 @@
 use lambda::{handler_fn, Context};
+use reqwest;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
+use tracing::{info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
+#[derive(Serialize, Deserialize, Debug)]
+struct FishnetStatus {
+    analysis: FishnetAnalysis,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FishnetAnalysis {
+    user: FishnetQueueMetric,
+    system: FishnetQueueMetric,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct FishnetQueueMetric {
+    acquired: u64,
+    queued: u64,
+    oldest: u64,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Error> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber).expect("Could not set global subscriber");
     let func = handler_fn(func);
     lambda::run(func).await?;
     Ok(())
 }
 
 async fn func(event: Value, _: Context) -> Result<Value, Error> {
+    let status = reqwest::get("https://lichess.org/fishnet/status")
+        .await?
+        .json::<FishnetStatus>()
+        .await?;
+    info!("got status: {:?}", status);
     Ok(event)
 }
