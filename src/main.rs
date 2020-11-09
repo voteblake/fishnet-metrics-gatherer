a4839db69f6aa8184580a65d0aa0270c44507a7c
Blake Johnson <johnsonblake1@gmail.com>
2020-11-09 10:30:57 -0800

    push queued metric for each dimension

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,37 @@
 use lambda::{handler_fn, Context};
+use once_cell::sync::OnceCell;
 use reqwest;
+use rusoto_cloudwatch::{CloudWatch, CloudWatchClient, Dimension, MetricDatum, PutMetricDataInput};
+use rusoto_core::Region;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use tracing::{info, Level};
 use tracing_subscriber::FmtSubscriber;
+use Default;
 
 type Error = Box<dyn std::error::Error + Send + Sync + 'static>;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FishnetStatus {
-    analysis: FishnetAnalysis,
+    pub analysis: FishnetAnalysis,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FishnetAnalysis {
-    user: FishnetQueueMetric,
-    system: FishnetQueueMetric,
+    pub user: FishnetQueueMetric,
+    pub system: FishnetQueueMetric,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
 struct FishnetQueueMetric {
-    acquired: u64,
-    queued: u64,
-    oldest: u64,
+    pub acquired: u64,
+    pub queued: u64,
+    pub oldest: u64,
+}
+
+fn cloudwatch_client() -> &'static CloudWatchClient {
+    static INSTANCE: OnceCell<CloudWatchClient> = OnceCell::new();
+    INSTANCE.get_or_init(|| CloudWatchClient::new(Region::default()))
 }
 
 #[tokio::main]
@@ -42,5 +51,32 @@ async fn func(event: Value, _: Context) -> Result<Value, Error> {
         .json::<FishnetStatus>()
         .await?;
     info!("got status: {:?}", status);
+
+    let metrics = PutMetricDataInput {
+        namespace: "fishnet".to_string(),
+        metric_data: vec![
+            MetricDatum {
+                metric_name: "queued".to_string(),
+                dimensions: Some(vec![Dimension {
+                    name: "queue".to_string(),
+                    value: "user".to_string(),
+                }]),
+                value: Some(status.analysis.user.queued as f64),
+                ..Default::default()
+            },
+            MetricDatum {
+                metric_name: "queued".to_string(),
+                dimensions: Some(vec![Dimension {
+                    name: "queue".to_string(),
+                    value: "system".to_string(),
+                }]),
+                value: Some(status.analysis.system.queued as f64),
+                ..Default::default()
+            },
+        ],
+    };
+
+    cloudwatch_client().put_metric_data(metrics).await?;
+
     Ok(event)
 }
